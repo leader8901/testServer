773f06b2e5cd6e2fdf055e5c859054ba808f0816
leader8901 <77359914+leader8901@users.noreply.github.com>
2022-10-04 18:01:11 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def sample():
+    return jsonify(
+        message=f'This is the {os.environ["APP"]} application.',
+        server=request.base_url,
+        custom_header=request.headers.get('MyCustomHeader', None),
+        host_header=request.headers.get('Host', request.base_url),
+        custom_params=request.args.get('MyCustomParam', None),
+        query_strings=request.query_string.decode('utf-8'),
+    )
+
+
+@app.route('/healthcheck')
+def healthcheck():
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ loadbalancer.py
@@ -0,0 +1,46 @@
+from random import random
+
+import requests
+from flask import Flask, request
+
+from utils import (
+    get_healthy_server,
+    healthcheck,
+    load_configuration,
+    process_firewall_rules_flag,
+    process_rules,
+    process_rewrite_rules,
+    transform_backends_from_config,
+)
+
+loadbalancer = Flask(__name__)
+
+MAIL_BACKENDS = ['localhost:8081', 'localhost:8082']
+YANDEX_BACKENDS = ['localhost:9081', 'localhost:9082']
+
+config = load_configuration('loadbalancer.yaml')
+register = transform_backends_from_config(config)
+
+
+@loadbalancer.route('/')
+def router():
+    host_header = request.headers['Host']
+    if host_header == 'www.mail.ru':
+        response = requests.get(f'http://{random.choice(MAIL_BACKENDS)}')
+        return response.content, response.status_code
+    elif host_header == 'www.yandex.ru':
+        response = requests.get(f'http://{random.choice(YANDEX_BACKENDS)}')
+        return response.content, response.status_code
+    else:
+        return 'Not Found', 404
+
+@loadbalancer.route('/mail')
+def mmail_path():
+    response = requests.get(f'http://{random.choice(MAIL_BACKENDS)}')
+    return response.content, response.status_code
+
+
+@loadbalancer.route('/yandex')
+def yandex_path():
+    response = requests.get(f'http://{random.choice(YANDEX_BACKENDS)}')
+    return response.content, response.status_code
